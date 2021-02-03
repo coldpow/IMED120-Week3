d946ac0a0b6a5f413e97422b8f595a2a4a8bcd21
Kathy Ly <60831517+coldpow@users.noreply.github.com>
2021-02-02 22:18:36 -0500

    Denmark + Congo Flag
    Added Denmark and Congo Flag to sketch.js

--- Week 4 - Shapes & Colors/sketch.js
+++ Week 4 - Shapes & Colors/sketch.js
@@ -1,9 +1,32 @@
 function setup() {
   // put setup code here
     createCanvas(innerWidth, innerHeight);
-    background ('orange');
+    background('grey');
 }
-
+    
 function draw() {
-  // put drawing code here
+    //Denmark Flag
+    noStroke();
+    fill('red');
+    rect(30,20,600,400);
+    fill('white');
+    rect(175,20,80,400);
+    rect(30,175,600,80);
+    
+    textSize(36);
+    fill(225);
+    text('Denmark Flag', 30, 475);
+    
+    //Congo Flag
+    noStroke();
+    fill('gold');
+    rect(650,20,600,400);
+    fill('green');
+    triangle(650,20,650,420,1050,20);
+    fill('red');
+    triangle(1250,20,850,420,1250,420);
+    
+    textSize(36);
+    fill(225);
+    text('Congo Flag', 650, 475);
 }
